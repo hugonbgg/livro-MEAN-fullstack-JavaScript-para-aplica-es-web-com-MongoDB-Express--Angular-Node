50c09de4866e82fa53c9d46e9a15a24706622a09
hugonbgg <hugonbgg@gmail.com>
2022-07-05 16:13:46 -0300

    Commit inicial

---
+++ contatooh/config/express.js
@@ -0,0 +1,31 @@
+//config/express.js
+
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+module.exports = function() {
+    var app = express();
+
+//variável de ambiente
+app.set('port',3000);
+
+//middleware
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views', './app/views')
+
+//novos middlewares(body-parser)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('method-override')());
+
+
+load('models', {cwd: 'app'})
+.then('controllers')
+.then('routes')
+.into(app);
+
+
+return app;
+};
